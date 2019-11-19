efb5639c86b433b58d657676b5246f01689329f8
dean-houari <57886895+dean-houari@users.noreply.github.com>
2019-11-19 14:21:41 +0900

    Update setup-hosts.sh

--- LAB/Vagrant/ubuntu/vagrant/setup-hosts.sh
+++ LAB/Vagrant/ubuntu/vagrant/setup-hosts.sh
@@ -9,9 +9,9 @@ sed -e '/^.*ubuntu-bionic.*/d' -i /etc/hosts
 
 # Update /etc/hosts about other hosts
 cat >> /etc/hosts <<EOF
-192.168.5.11  master-1
-192.168.5.12  master-2
-192.168.5.21  worker-1
-192.168.5.22  worker-2
-192.168.5.30  lb
+192.168.50.101  master-1
+192.168.50.102  master-2
+192.168.50.201  minion-1
+192.168.50.202  minion-2
+192.168.50.30  lb
 EOF
